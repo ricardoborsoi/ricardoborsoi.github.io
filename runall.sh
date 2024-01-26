6b52a7316b47b33905a6e4a77341013dcc4d7c1b
ricardoborsoi <raborsoi@gmail.com>
2024-01-26 22:19:18 +0100

    update projects

--- runall.sh
+++ runall.sh
@@ -4,5 +4,6 @@ python jemdoc_mathjax/jemdoc -c mysite.conf publications.jemdoc
 python jemdoc_mathjax/jemdoc -c mysite.conf codes.jemdoc
 python jemdoc_mathjax/jemdoc -c mysite.conf research.jemdoc
 python jemdoc_mathjax/jemdoc -c mysite.conf people.jemdoc
-
+python jemdoc_mathjax/jemdoc -c mysite.conf p_LENTILLE.jemdoc
+python jemdoc_mathjax/jemdoc -c mysite.conf p_AGDAM.jemdoc
 
